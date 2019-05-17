b316317bb0af7e1e9c2071072f6d078c42a986db
blukitas <Lucasbertolini@hotmail.com>
2019-05-17 19:16:15 -0300

    OTro ejercicio resuelto

--- Python/remove-not-letters.py
+++ Python/remove-not-letters.py
@@ -8,3 +8,15 @@
 #   * remove_chars('1 + 1 = 2')    ==> '    ' (string with 4 spaces)
 #   * remove_chars("0123456789(.)+,|[]{}=@/~;^$'<>?-!*&:#%_")  ==> '' (empty string)
 #   https://www.codewars.com/kata/59be6bdc4f98a8a9c700007d
+import re
+
+
+# print((x for x in cadena if re.match(r"[azAZ]", x)))
+
+cadena = 'my_list = ["a","b","c"]'
+salida = ''
+for c in cadena:
+    n = ord(c)
+    if ord('a') <= n <= ord('z') or ord('A') <= n <= ord('Z') or n == ord(' '):
+        salida += chr(n)
+print salida
